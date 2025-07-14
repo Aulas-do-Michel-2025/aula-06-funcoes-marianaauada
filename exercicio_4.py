cfba9b30303dc1ed7ec446231bf73bb44464f9bc
marianaauada <marianabelloauada@gmail.com>
2025-07-14 20:40:59 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -18,7 +18,18 @@ def verificar_se_eh_primo(numero):
     return False
 
 
+def verificar_se_eh_primo(numero):
+    if numero > 1:
+        for i in range(2, int(numero / 2) + 1):
+            if (numero % i) == 0:
+                return False
+        return True
+    return False
+
+
 def filtrar_lista_por_numeros_primos(lista):
     lista_filtrada = []
-    # Completar funcao aqui!
+    for numero in lista:
+        if verificar_se_eh_primo(numero):
+            lista_filtrada.append(numero)
     return lista_filtrada
